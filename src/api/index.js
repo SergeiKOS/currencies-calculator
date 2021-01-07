e0ca4153a9f4d4582a7b5779f023d89c0707d490
Sergei Kostrikin <cptlbc@yandex.ru>
2021-01-06 20:32:47 -0500

    Small changes

--- src/api/index.js
+++ src/api/index.js
@@ -3,10 +3,10 @@ import axios from "axios";
 const url = "https://api.exchangeratesapi.io";
 
 export const fetchUSDCADRate = async () => {
-  let changableUrl = `${url}/latest?base=USD&symbols=USD,CAD`;
+  let changeableUrl = `${url}/latest?base=USD&symbols=USD,CAD`;
 
   try {
-    return await axios.get(changableUrl);
+    return await axios.get(changeableUrl);
   } catch (error) {
     console.log(error);
   }
